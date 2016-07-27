8eb44bb12788ae0358881ca97be310377ea86086
Drew Conly <atconly@gmail.com>
2016-07-27 11:16:34 -0600

    bring in canvasjs and add styling

--- scripts/followCtrl.app.js
+++ scripts/followCtrl.app.js
@@ -24,8 +24,6 @@ function followCtrl(dollarFactory, predictionFactory) {
     follow.prediction = response.data.data[0];
   })
 
-  window.follow = follow;
-
   follow.countdown = function() {
     var currentDate  = new Date();
     var electionDate = new Date(Date.UTC(2016, 10, 8, 7, 0, 0))
@@ -35,6 +33,32 @@ function followCtrl(dollarFactory, predictionFactory) {
     		clockFace: 'DailyCounter',
     		countdown: true
     	});
+  }
+
+  follow.chartRender = function() {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      title:{
+        text: "PollyVote Prediction",
+        verticalAlign: 'top',
+        horizontalAlign: 'left'
+      },
+      animationEnabled: true,
+      data: [
+        {
+          type: "doughnut",
+          startAngle:20,
+          toolTipContent: "{label}: {y} - <strong>#percent%</strong>",
+          indexLabel: "{label} #percent%",
+          dataPoints: [
+            {  y: 49, label: "Dem" },
+            {  y: 49, label: "Rep" },
+            {  y: 2, label: "Other/undecided" }
+          ]
+        }
+      ]
+    });
+    chart.render();
   }
 
 }
